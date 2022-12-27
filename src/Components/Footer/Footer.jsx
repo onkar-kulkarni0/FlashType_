9c43b4caaa65b9bd2182a478b6948bec5c36a3e3
onkar-kulkarni0 <onkarkulkarni3107@gmail.com>
2022-12-27 23:19:13 +0530

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <a href="https://github.com/onkar-kulkarni0"
+                className="footer-link"
+                target="_blank"
+                rel="noreferrer"
+            >
+                Visit my github
+            </a>
+        </div>
+    )
+}
+export default Footer;
